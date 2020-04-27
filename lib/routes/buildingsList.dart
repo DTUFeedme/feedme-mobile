7019d9f3e975e6465ba7bc41bb091632f3b85969
BettaKalisa <46486945+BettaKalisa@users.noreply.github.com>
2020-04-27 16:41:00 +0200

    admin-panel

--- lib/routes/buildingsList.dart
+++ lib/routes/buildingsList.dart
@@ -2,6 +2,7 @@ import 'package:climify/models/api_response.dart';
 import 'package:climify/models/buildingModel.dart';
 import 'package:climify/models/globalState.dart';
 import 'package:climify/services/rest_service.dart';
+import 'package:direct_select/direct_select.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,6 +14,9 @@ class BuildingsList extends StatefulWidget {
 class _BuildingsListState extends State<BuildingsList> {
   RestService _restService = RestService();
   List<BuildingModel> _buildings = [];
+  int _visibleIndex = 0;
+  List<DropdownMenuItem<BuildingModel>> _dropdownMenuItems;
+  BuildingModel _selectedBuilding;
 
   @override
   void initState() {
@@ -20,8 +24,27 @@ class _BuildingsListState extends State<BuildingsList> {
     _getBuildings();
   }
 
-  void _getBuildings() async {
-    await Future.delayed(Duration.zero);
+  List<DropdownMenuItem<BuildingModel>> buildDropdownMenuItems(List<BuildingModel> buildings) {
+    List<DropdownMenuItem<BuildingModel>> items = List();
+    for (BuildingModel building in buildings) {
+      items.add(
+        DropdownMenuItem(
+          value: building, 
+          child: Text(building.name),
+        ),
+      );
+    }
+    return items;
+  }
+
+  void _changeWindow(int index) {
+    setState(() {
+      _visibleIndex = index;
+    });
+  }
+
+  Future<void> _getBuildings() async {
+    //await Future.delayed(Duration.zero);
     String token = Provider.of<GlobalState>(context).globalState['token'];
     APIResponse<List<BuildingModel>> buildingsResponse =
         await _restService.getBuildingsWithAdminRights(token);
@@ -36,6 +59,69 @@ class _BuildingsListState extends State<BuildingsList> {
     Navigator.of(context).pushNamed("buildingManager");
   }
 
+  onChangeDropdownItem(BuildingModel selectedBuilding){
+    setState(() {
+      _selectedBuilding = selectedBuilding;
+    });
+  }
+
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Administration",
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.lock_open),
+            title: Text("Buildings"),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle_outline), 
+              title: Text("Add beacon"))
+        ],
+        onTap: (int index) => _changeWindow(index),
+        currentIndex: _visibleIndex,
+      ),
+      body: Container(
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Visibility(
+              visible: _visibleIndex == 0,
+              child: Container(
+                child: ListView.builder(
+                  itemCount: _buildings.length,
+                  itemBuilder: (context, index) => _buildingRow(_buildings[index]),
+                ),
+              ),
+            ),
+            Visibility(
+              visible: _visibleIndex == 1,
+              child: Column(
+                children: <Widget>[
+                  Text("Select building"),
+                  SizedBox(height: 20.0,),
+                  DropdownButton(
+                    value: _dropdownMenuItems[0].value,
+                    items: buildDropdownMenuItems(_buildings),
+                    onChanged: onChangeDropdownItem,
+                  ),
+                  SizedBox(height: 20.0,),
+                  Text('Selected: ${_selectedBuilding.name}')
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+/*
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,7 +136,7 @@ class _BuildingsListState extends State<BuildingsList> {
       ),
     );
   }
-
+*/
   Widget _buildingRow(BuildingModel building) {
     return InkWell(
       onTap: () => _focusBuilding(building),
