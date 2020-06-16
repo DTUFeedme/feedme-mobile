f205a4eecaafd365f431bd3bfb7870ba6a194e57
s174268 <s174268@student.dtu.dk>
2020-06-16 14:01:55 +0200

    addbeacon auto

--- lib/routes/dialogues/questionMenu.dart
+++ lib/routes/dialogues/questionMenu.dart
@@ -3,13 +3,14 @@ import 'package:climify/models/beacon.dart';
 import 'package:climify/models/buildingModel.dart';
 import 'package:climify/models/roomModel.dart';
 import 'package:climify/models/questionModel.dart';
+import 'package:climify/models/feedbackQuestion.dart';
 import 'package:climify/services/rest_service.dart';
 import 'package:climify/services/snackbarError.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class QuestionMenu {
-  Question question;
+  FeedbackQuestion question;
   String token;
   GlobalKey<ScaffoldState> scaffoldKey;
   Function(String) setCurrentlyConfirming;
@@ -18,16 +19,16 @@ class QuestionMenu {
 
   RestService _restService = RestService();
 
-  // Future<void> _deleteQuestion() async {
-  //   APIResponse<String> deleteResponse =
-  //       await _restService.deleteBeacon(token, beacon.id, building);
-  //   if (!deleteResponse.error) {
-  //     SnackBarError.showErrorSnackBar("Question ${question.value} set inactive", scaffoldKey);
-  //   } else {
-  //     SnackBarError.showErrorSnackBar(deleteResponse.errorMessage, scaffoldKey);
-  //   }
-  //   return;
-  // }
+  Future<void> _makeQuestionInactive() async {
+    APIResponse<FeedbackQuestion> deleteResponse =
+        await _restService.makeQuestionInactive(token, question.id);
+    if (!deleteResponse.error) {
+      SnackBarError.showErrorSnackBar("Question ${question.value} set inactive", scaffoldKey);
+    } else {
+      SnackBarError.showErrorSnackBar(deleteResponse.errorMessage, scaffoldKey);
+    }
+    return;
+  }
 
   QuestionMenu({
     this.question,
@@ -43,11 +44,11 @@ class QuestionMenu {
           title: Text("${question.value}"),
           children: <Widget>[
             getCurrentlyConfirming() == "inactivequestion"
-                ? RaisedButton(
+                ? RaisedButton( 
                     color: Colors.red,
                     child: Text("Confirm"),
                     onPressed: () async {
-                      //await _deleteQuestion();
+                      await _makeQuestionInactive();
                       Navigator.of(context).pop();
                     },
                   )
