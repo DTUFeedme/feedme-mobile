fb6d3c592209a61d5c8b6ddf4717714ed82be36d
SebastianKotewitz <siggibastian@hotmail.com>
2020-06-16 14:28:30 +0200

    fixed beacons not being selected

--- lib/routes/dialogues/addBeacon.dart
+++ lib/routes/dialogues/addBeacon.dart
@@ -24,34 +24,35 @@ class AddBeacon {
     this.building,
     this.scaffoldKey,
   }) {
+    List<bool> list = [];
+    for (int i = 0; i < beaconList.length; i++) {
+      list.add(false);
+    }
     addBeaconDialog = StatefulBuilder(
       builder: (context, setState) {
-        List<bool> list = [];
-        for (int i = 0; i < beaconList.length; i++){
-          list.add(false);
-        } 
         void _submitBeacon() async {
-          for(int i = 0; i < beaconList.length; i++){
-            if(list[i] == true){
-            APIResponse<bool> apiResponse = await _restService.addBeacon(
-            token,
-            Tuple2(beaconList[i].item1,beaconList[i].item2),
-            building,
-          );
-          if (apiResponse.error == false) {
-            SnackBarError.showErrorSnackBar(
-                //${apiResponse.data.name}
-                "Beacon added", scaffoldKey);
-            Navigator.of(context).pop(true);
-          } else {
-            SnackBarError.showErrorSnackBar(
-                apiResponse.errorMessage, scaffoldKey);
-            Navigator.of(context).pop(false);
-          }
+          for (int i = 0; i < beaconList.length; i++) {
+            if (list[i] == true) {
+              APIResponse<bool> apiResponse = await _restService.addBeacon(
+                token,
+                Tuple2(beaconList[i].item1, beaconList[i].item2),
+                building,
+              );
+              if (apiResponse.error == false) {
+                SnackBarError.showErrorSnackBar(
+                    //${apiResponse.data.name}
+                    "Beacon added",
+                    scaffoldKey);
+                Navigator.of(context).pop(true);
+              } else {
+                SnackBarError.showErrorSnackBar(
+                    apiResponse.errorMessage, scaffoldKey);
+                Navigator.of(context).pop(false);
+              }
             }
           }
         }
-        
+
         bool submitEnabled = false;
 
         // bool isSelected = false;
@@ -62,56 +63,56 @@ class AddBeacon {
             list[index] = false;
           });
         }
+
         void updateSelectedBeaconListAdd(int index) async {
           setState(() {
             list[index] = true;
           });
         }
-        
+
         //return CircularProgressIndicator();
 
-        
         return SimpleDialog(
           title: Text("Add Beacon"),
           children: <Widget>[
-              Container(
-                width: double.maxFinite,
+            Container(
+              width: double.maxFinite,
               child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemCount: beaconList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return InkWell(
-                          onLongPress: () {
-                                //beaconList.any((item) => item.isSelected)
-                                if (list[index] == true) {
-                                //setState(() {
-                                  //beaconList[index].isSelected = !beaconList[index].isSelected;
-                                  updateSelectedBeaconListRemove(index);
-                                  //list[index] = false;
-                                //});
-                            }
-                          },
-                          onTap: () {
-                                //setState(() {
-                                      //beaconList[index].isSelected = true;
-                                      updateSelectedBeaconListAdd(index);
-                                      //list[index] = true;
-                                //});
-                            },
-                          child: Container(
-                            margin: EdgeInsets.symmetric(vertical: 8),
-                            color: //list[index].isSelected ? 
-                                    list[index] == true ? 
-                                      Colors.grey[400] : Colors.white,
-                            child: ListTile(
-                                    title: Text(beaconList[index].item1),
-                                  ),
-                            ),
-                  );
-                }
-              ),
-              ),
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: beaconList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                      onLongPress: () {
+                        //beaconList.any((item) => item.isSelected)
+                        if (list[index] == true) {
+                          //setState(() {
+                          //beaconList[index].isSelected = !beaconList[index].isSelected;
+                          updateSelectedBeaconListRemove(index);
+                          //list[index] = false;
+                          //});
+                        }
+                      },
+                      onTap: () {
+                        //setState(() {
+                        //beaconList[index].isSelected = true;
+                        updateSelectedBeaconListAdd(index);
+                        //list[index] = true;
+                        //});
+                      },
+                      child: Container(
+                        margin: EdgeInsets.symmetric(vertical: 8),
+                        color: //list[index].isSelected ?
+                            list[index] == true
+                                ? Colors.grey[400]
+                                : Colors.white,
+                        child: ListTile(
+                          title: Text(beaconList[index].item1),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
             RaisedButton(
               color: submitEnabled ? Colors.green : Colors.red,
               child: Text("Submit"),
