3b49fd0434f1c5f1cbf2dd96d270ab72baaf6d34
SebastianKotewitz <siggibastian@hotmail.com>
2020-09-29 16:20:37 +0200

    Made the notification more consistent. It no longer closes the top nav bar after scanning if it is the only notification.

--- lib/services/send_receive_location.dart
+++ lib/services/send_receive_location.dart
@@ -14,9 +14,6 @@ Future<APIResponse<RoomModel>> sendReceiveLocation() async {
   }
 
   String notificationTitle = "Scanning room";
-  Future.delayed(Duration(milliseconds: 200)).then(
-    (_) => LocalNotifications.flutterLocalNotificationsPlugin.cancel(0),
-  );
 
   var androidPlatformChannelSpecifics = AndroidNotificationDetails(
     'Periodic Scan Notification',
@@ -24,6 +21,7 @@ Future<APIResponse<RoomModel>> sendReceiveLocation() async {
     'Notification showing the last room estimation result. The notification can be tapped to update your current position. This allows the system to perform personalized updates on the indoor climate',
     enableVibration: false,
     playSound: false,
+    autoCancel: false,
     color: const Color.fromARGB(255, 25, 155, 255),
     channelAction: AndroidNotificationChannelAction.CreateIfNotExists,
     category: 'alarm',
@@ -35,33 +33,28 @@ Future<APIResponse<RoomModel>> sendReceiveLocation() async {
   var iOSPlatformChannelSpecifics = IOSNotificationDetails();
   var platformChannelSpecifics = NotificationDetails(
       androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-  Future.delayed(Duration(milliseconds: 450)).then(
-    (_) => LocalNotifications.flutterLocalNotificationsPlugin.show(
-      0,
-      notificationTitle,
-      "",
-      platformChannelSpecifics,
-    ),
+  LocalNotifications.flutterLocalNotificationsPlugin.show(
+    0,
+    notificationTitle,
+    "",
+    platformChannelSpecifics,
   );
 
   BluetoothServices _bluetoothServices = BluetoothServices();
 
   APIResponse<RoomModel> apiResponse =
       await _bluetoothServices.getRoomFromScan();
-  LocalNotifications.flutterLocalNotificationsPlugin.cancel(0);
   RoomModel _room = apiResponse?.data;
 
   notificationTitle = _room?.name != null
       ? "Current room: ${_room.name}"
       : "Couldn't scan room";
-  await Future.delayed(Duration(milliseconds: 250)).then(
-    (_) => LocalNotifications.flutterLocalNotificationsPlugin.show(
-      0,
-      notificationTitle,
-      "Tap to rescan room",
-      platformChannelSpecifics,
-      payload: "scan",
-    ),
+  LocalNotifications.flutterLocalNotificationsPlugin.show(
+    0,
+    notificationTitle,
+    "Tap to rescan room",
+    platformChannelSpecifics,
+    payload: "scan",
   );
 
   LocalNotifications.preventSelectNotification = false;
